85f364ab1ecd4180c716577986781862ab62193d
Adrian Abella <fasttaco1@gmail.com>
2023-03-09 17:33:10 -0500

    Updated Stopwatch Time Capacity

---
+++ script.js
@@ -0,0 +1,7 @@
+// Target buttons
+const startButton = document.querySelector("#start-button");
+const stopButton = document.querySelector("#stop-button");
+const splitButton = document.querySelector("#split-button");
+const resetButton = document.querySelector("#reset-button");
+
+// Target individual
